36a5f3e9683848f6240f5e4b6dd2ed37a69db773
Abdulrauf-Laydi <abdalruof.khl@gmail.com>
2024-11-09 15:50:39 +0300

    first commit

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Person {
+    private String name;
+    private int id;
+
+    public Person(int id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
